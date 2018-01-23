3f1f4c4bf8fa5f530324a9a5efd54d8b83e60d13
Roy Talyosef <roy@productionmap.com>
2018-01-23 11:53:22 +0200

    PMG-309 First run screen Checking request post parameters and mongo connection

--- server/api/controllers/setup.controller.js
+++ server/api/controllers/setup.controller.js
@@ -1,5 +1,6 @@
 const fs = require('fs');
 const hooks = require('../../libs/hooks/hooks');
+const mongoose = require('mongoose');
 let dbconfig;
 try {
     dbconfig = require("../../env/dbconfig");
@@ -15,16 +16,34 @@ module.exports = {
         }
         return res.send(false)
     },
+
     setupDbConnectionString: (req, res) => {
-        console.log(req.body);
+
         const dbDetails = req.body;
-        fs.writeFile('./env/dbconfig.json', JSON.stringify(dbDetails), (err) => {
-            if (err) {
-                return res.status(500).send(err);
-            }
-            console.log('The file has been saved!');
-            dbconfig = require("../../env/dbconfig");
-            return res.send("OK");
+        let connectionString;
+        if (!dbDetails.uri && !(dbDetails.url && dbDetails.port && dbDetails.name)) {
+            return res.status(500).send('Missing parameters')
+        }
+        if (dbDetails.username && dbDetails.password) {
+            connectionString = `mongodb://${dbDetails.username}:${dbDetails.password}@${dbDetails.url}:${dbDetails.port}/${dbDetails.name}`;
+        } else {
+            connectionString = `mongodb://${dbDetails.url}:${dbDetails.port}/${dbDetails.name}`;
+        }
+
+        mongoose.connect((dbDetails.uri || connectionString), {
+            useMongoClient: true
+        }).then(() => {
+            console.log(`Succesfully Connected to the Mongodb Database  at URL : mongodb://127.0.0.1:27017/refactor`);
+            fs.writeFile('./env/dbconfig.json', JSON.stringify(dbDetails), (err) => {
+                if (err) {
+                    throw new Error(err);
+                }
+                dbconfig = require("../../env/dbconfig");
+                return res.send("OK");
+            });
+        }).catch((error) => {
+            return res.status(500).send((error || `Error Connecting to the Mongodb`));
         });
+
     }
 };
