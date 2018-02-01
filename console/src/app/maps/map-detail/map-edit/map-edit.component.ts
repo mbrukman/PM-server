a222191f0014574e2ad48ad0ac3fa71c0bba6769
Roy Talyosef <roy@productionmap.com>
2018-02-01 11:30:00 +0200

    PMG-319 Import & Export map Added export map to file

--- console/src/app/maps/map-detail/map-edit/map-edit.component.ts
+++ console/src/app/maps/map-detail/map-edit/map-edit.component.ts
@@ -7,7 +7,6 @@ import { MapsService } from '../../maps.service';
 import { Map } from '../../models/map.model';
 import { MapStructure } from '../../models/map-structure.model';
 import { MapDesignService } from './map-design.service';
-import { ImportExportComponent } from './import-export/import-export.component';
 
 @Component({
   selector: 'app-map-edit',
@@ -41,47 +40,6 @@ export class MapEditComponent implements OnInit, OnDestroy {
   ngOnDestroy() {
     this.mapSubscription.unsubscribe();
     this.mapStructureSubscription.unsubscribe();
-
-  }
-
-  openImportExportModal() {
-    let structure = Object.assign({}, this.mapStructure);
-    structure = this.removePrivateFields(structure);
-    const modal = this.modalService.show(ImportExportComponent);
-    modal.content.mapStructure = JSON.stringify(structure);
-    modal.content.result
-      .take(1)
-      .filter(result => result)
-      .subscribe(result => {
-        this.mapsService.setCurrentMapStructure(JSON.parse(result))
-      });
-  }
-
-  removePrivateFields(structure) {
-    delete structure.map;
-    delete structure._id;
-    delete structure.id;
-
-    structure.processes.forEach((process, i) => {
-      delete structure.processes[i]._id;
-      delete structure.processes[i].plugin;
-      delete structure.processes[i].createdAt;
-      delete structure.createdAt;
-
-      if (process.actions) {
-        process.actions.forEach((action, j) => {
-          delete structure.processes[i].actions[j]._id;
-          delete structure.processes[i].actions[j].id;
-        });
-      }
-    });
-
-    structure.links.forEach((link, i) => {
-      delete structure.links[i]._id;
-      delete structure.links[i].createdAt;
-    });
-
-    return structure;
 
   }
 
