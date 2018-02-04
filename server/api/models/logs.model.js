fc4ab7388cc699460c939d524807ece9a6b169b2
Roy Talyosef <roy@productionmap.com>
2018-02-04 17:16:07 +0200

    PMG-328 Server - Logger created log schema

---
+++ server/api/models/logs.model.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+let logSchema = new Schema({
+    message: String,
+    status: String,
+    meta: Schema.Types.Mixed,
+    stacktrace: Schema.Types.Mixed,
+    timestamp: Date
+});
+
+
+let Log = mongoose.model('Log', logSchema, 'log');
+
+
+module.exports = Map;
