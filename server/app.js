b8e83b6a1dc75f1876a4eafe30669058bce0b4e9
Roy Talyosef <roy@productionmap.com>
2018-02-06 10:11:46 +0200

    PMG-328 Server - Logger add mongo transport to express winston

--- server/app.js
+++ server/app.js
@@ -7,6 +7,8 @@ const expressWinston = require('express-winston');
 const mongoose = require('mongoose');
 const bootstrap = require("./helpers/bootstrap").bootstrap;
 const socket = require('socket.io');
+const winstonMongo = require('winston-mongodb');
+
 const env = require('./env/enviroment');
 const app = express();
 
@@ -24,17 +26,6 @@ app.use((req, res, next) => {
 });
 
 // winston logger
-app.use(expressWinston.logger({
-    transports: [
-        new winston.transports.Console({
-            json: false,
-            colorize: true
-        })
-    ],
-    meta: false, // optional: control whether you want to log the meta data about the request (default to true)
-    expressFormat: true, // Use the default Express/morgan request formatting. Enabling this will override any msg if true. Will only output colors with colorize set to true
-    colorize: false, // Color the text and status code, using the Express/morgan color palette (text: gray, status: default green, 3XX cyan, 4XX yellow, 5XX red).
-}));
 
 
 const server = http.createServer(app);
@@ -43,14 +34,31 @@ io.on('connection', function (socket) {
     console.log('a user connected');
 });
 
+let expressWinstonTranports = [
+    new winston.transports.Console({
+        json: false,
+        colorize: true
+    })];
 if (env.dbURI) {
-    console.log('--', env.dbURI);
     mongoose.connect(env.dbURI, {
         useMongoClient: true
     }).then(() => {
-        console.log(`Succesfully Connected to the Mongodb Database`);
+        winston.add(winstonMongo.MongoDB, {
+            db: env.dbURI,
+
+        });
+        winston.log('info', `Succesfully Connected to the Mongodb at ${env.dbURI}`);
     });
+    expressWinstonTranports.push(new winston.transports.MongoDB({db: env.dbURI}));
 }
+
+app.use(expressWinston.logger({
+    transports: expressWinstonTranports,
+    meta: true, // optional: control whether you want to log the meta data about the request (default to true)
+    msg: "HTTP {{req.method}} {{req.url}} {{req.statusCode}}",
+    expressFormat: false, // Use the default Express/morgan request formatting. Enabling this will override any msg if true. Will only output colors with colorize set to true
+    colorize: false, // Color the text and status code, using the Express/morgan color palette (text: gray, status: default green, 3XX cyan, 4XX yellow, 5XX red).
+}));
 
 mongoose.Promise = require('bluebird');
 
@@ -61,6 +69,7 @@ app.use(bodyParser.json());
 
 app.use((req, res, next) => {
     req.io = io;
+    req.app = app;
     next();
 });
 
