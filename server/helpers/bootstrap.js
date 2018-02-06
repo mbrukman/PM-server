debc466c712c1368eafae29a77fce733579d2aa3
Roy Talyosef <roy@productionmap.com>
2018-02-06 10:41:13 +0200

    PMG-328 Server - Logger Added missing import statement

--- server/helpers/bootstrap.js
+++ server/helpers/bootstrap.js
@@ -1,3 +1,4 @@
+const winston = require("winston");
 const agentsService = require("../api/services/agents.service");
 const pluginsService = require("../api/services/plugins.service");
 const scheduledJobsService = require("../api/services/scheduled-job.service");
