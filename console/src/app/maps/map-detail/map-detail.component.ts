d18c76e319644a493d20e24b649fa375211f879d
kadosh1000 <kadosh10001@gmail.com>
2018-11-18 14:28:57 +0200

    * add support for ctrl+s from all map views

--- console/src/app/maps/map-detail/map-detail.component.ts
+++ console/src/app/maps/map-detail/map-detail.component.ts
@@ -15,7 +15,10 @@ import { SocketService } from '@shared/socket.service';
 @Component({
   selector: 'app-map-detail',
   templateUrl: './map-detail.component.html',
-  styleUrls: ['./map-detail.component.scss']
+  styleUrls: ['./map-detail.component.scss'],
+  host : {
+    '(document:keydown)': 'onKeyDown($event)'
+  }
 })
 export class MapDetailComponent implements OnInit, OnDestroy {
   id: string;
@@ -165,6 +168,7 @@ export class MapDetailComponent implements OnInit, OnDestroy {
       this.executing = maps.indexOf(this.id) > -1;
     });
   }
+
   ngOnDestroy() {
     this.routeReq.unsubscribe();
     this.mapReq.unsubscribe();
@@ -353,7 +357,7 @@ export class MapDetailComponent implements OnInit, OnDestroy {
     this.mapsService.setCurrentMapStructure(this.mapStructure);
   }
 
-  onKeyDown($event) {
+  onKeyDown($event: KeyboardEvent) {
     let charCode = String.fromCharCode($event.which).toLowerCase();
     if ($event.ctrlKey && charCode === 's') {
         // Action on Ctrl + S
