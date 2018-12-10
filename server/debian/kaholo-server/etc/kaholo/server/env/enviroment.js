9a874f16fdbd7f18f7f0b159abd1448b1189646b
kadosh1000 <kadosh10001@gmail.com>
2018-12-10 14:03:50 +0200

    * remove files

--- server/debian/kaholo-server/etc/kaholo/server/env/enviroment.js
+++
@@ -1,7 +0,0 @@
-const path = require("path");
-const config = require("./config");
-
-let static_cdn = {static_cdn: path.join(path.dirname(path.dirname(__dirname)), "static_cdn")}
-let env = Object.assign({}, static_cdn, config);
-
-module.exports = env;
