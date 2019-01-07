bb840c76892581b99986fd67cef18b82a489eef6
or_sharbat <or@kaholo.io>
2019-01-07 12:37:55 +0200

    KG-500
    Dashboard - displays archived maps

--- server/api/services/map-execution.service.js
+++ server/api/services/map-execution.service.js
@@ -1478,20 +1478,49 @@ module.exports = {
      */
     dashboard: () => {
         return MapResult.aggregate([
+            {
+                $lookup:
+                {
+                    from: "maps",
+                    let: { mapId: "$map" },
+                    pipeline: [
+                        {
+                            $match: {
+                                $expr: {
+                                    $eq: ["$$mapId", "$_id"]
+                                }
+                            }
+                        },
+                        {
+                            $project:
+                            {
+                                name: 1,
+                                archived: 1,
+                            }
+                        }
+                    ],
+                    as: "maps",
+                },
+            },
+            {
+                $unwind: {
+                    "path": "$maps",
+                    "preserveNullAndEmptyArrays": true
+                }
+            },
+            { $match: { "maps.archived": false } },
             { $sort: { "startTime": -1 } },
             {
                 "$group":
                 {
                     _id: "$map", count: { $sum: 1 },
                     exec: { $first: "$$CURRENT" },
-                    map: { $first: "$map" },
+                    map: { $first: "$maps" },
                 }
             },
             { $sort: { "exec.startTime": -1 } },
-            { $limit: 16 }
-        ]).then(res => {
-            return Map.populate(res, { path: 'map' })
-        })
+            { $limit: 16 },
+        ])
     },
 
     /**
