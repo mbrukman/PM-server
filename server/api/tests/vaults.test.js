8568df56ea65ccff0550640a887cc5f18b1805ea
chbernat <bernat.christopher@gmail.com>
2019-09-06 21:34:20 +0200

    added tests for helper functions

--- server/api/tests/vaults.test.js
+++ server/api/tests/vaults.test.js
@@ -1,11 +1,11 @@
 const {initTestDataManager, generateVaults} = require('./factories');
 const VaultModel = require('../../api/models/vault.model');
-const baseUrl = 'localhost:3000';
 const {setupDB} = require('./helpers/test-setup');
 const {sortBy} = require('lodash');
 const request = require('supertest');
+const {server} = require('../../app');
 
-setupDB('testing');
+setupDB('testing', server);
 
 describe("Get all vaults, GET /vaults", () => {
     const testDataManager = initTestDataManager(VaultModel);
@@ -23,8 +23,7 @@ describe("Get all vaults, GET /vaults", () => {
 
     it('Gets the list of all vaults in the system', async () => {
         try {
-            const response = await request(baseUrl).get(`/api/vault`)
-                .withCredentials();
+            const response = await request.agent(server).get(`/api/vault`);
             expect(response.status).toBe(200);
             expect(response.body.length).toEqual(testDataManager.collection.length);
             const expected = sortBy(response.body, 'key');
@@ -35,8 +34,27 @@ describe("Get all vaults, GET /vaults", () => {
                 expect(item.description).toBe(expected[idx].description);
             })
         } catch (err) {
-            console.log(err.message)
+            console.log(err.message);
             throw err;
         }
-    })
+    });
+
+    // it('Gets the given vault from the system', async () => {
+    //     try {
+    //         const response = await request.agent(server).get(`/api/vault/${}`);
+    //         expect(response.status).toBe(200);
+    //         expect(response.body.length).toEqual(testDataManager.collection.length);
+    //         const expected = sortBy(response.body, 'key');
+    //         const received = sortBy(testDataManager.collection, 'key');
+    //         received.forEach((item, idx) => {
+    //             expect(item.key).toBe(expected[idx].key);
+    //             expect(item.id).toBe(expected[idx].id);
+    //             expect(item.description).toBe(expected[idx].description);
+    //         })
+    //     } catch (err) {
+    //         console.log(err.message);
+    //         throw err;
+    //     }
+    // });
+
 });
