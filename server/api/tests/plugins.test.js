8a93c2539fcb233a12b5a1b196c3c4ec687b8699
arispen-desktop <arispen@gmail.com>
2019-09-09 15:27:04 +0200

    tests descriptions

--- server/api/tests/plugins.test.js
+++ server/api/tests/plugins.test.js
@@ -25,9 +25,64 @@ describe('Plugins tests', () => {
             });
         });
 
+        describe(`GET /`, () => {
+            it(`should respond with a list of plugins`, (done) => {
+
+            });
+        });
+
+        describe(`GET /:id`, () => {
+            it(`should respond with a plugin of a given id`, (done) => {
+
+            });
+        });
+
+        // notice: POST method used for "update" operation
+        describe(`POST /:id/settings`, () => {
+            it(`should respond with an updated plugin`, (done) => {
+
+            });
+        });
+
+        // notice: redundant "/delete"
+        describe(`DELETE /:id/delete`, () => {
+            it(`should respond with a plugin of a given id`, (done) => {
+
+            });
+        });
+
     });
 
     describe('Negative', () => {
 
+        describe(`POST /upload`, () => {
+            it(`should respond with status 500 and proper error msg`, (done) => {
+
+            });
+        });
+
+        describe(`GET /`, () => {
+            it(`should respond with status 500 and proper error msg`, (done) => {
+                // TODO: how to get 500 here?
+            });
+        });
+
+        describe(`GET /:id`, () => {
+            it(`should respond with status 500 and proper error msg`, (done) => {
+
+            });
+        });
+
+        describe(`POST /:id/settings`, () => {
+            it(`should respond with status 500 and proper error msg`, (done) => {
+
+            });
+        });
+
+        describe(`DELETE /:id/delete`, () => {
+            it(`should respond with status 500 and proper error msg`, (done) => {
+
+            });
+        });
     });
 });
