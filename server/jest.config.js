20d30c3a4790def088c6302fc3637ed83170061e
chbernat <bernat.christopher@gmail.com>
2019-09-09 10:09:41 +0200

    jest settings

--- server/jest.config.js
+++ server/jest.config.js
@@ -1,3 +1,12 @@
 module.exports = {
-    testEnvironment: 'node'
+    testEnvironment: 'node',
+    coveragePathIgnorePatterns: ["/node_modules/"], // default
+    coverageReporters: ["json", "clover"],
+    displayName: {
+        name: 'Node.js SERVER',
+        color: 'blue',
+    },
+    projects: ["./"],
+    collectCoverage: true
+    // coverageThreshold: https://github.com/facebook/jest/blob/master/docs/Configuration.md
 };
