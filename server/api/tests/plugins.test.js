4863a000a264b5507e728e84f09510ac1ac4ab4c
arispen-desktop <arispen@gmail.com>
2019-09-10 19:54:49 +0200

    positive tests

--- server/api/tests/plugins.test.js
+++ server/api/tests/plugins.test.js
@@ -1,8 +1,9 @@
 const request = require('supertest');
-const fs = require('fs');
-//const app = require('../../app');
+const app = require('../../app');
 
-//const file = fs.readFileSync('./files/kaholo-plugin.csv.zip');
+const pluginFile = './files/kaholo-plugin.csv.zip';
+// TODO: use factory
+let pluginID = '5d77df087044612270179e25';
 
 describe('Plugins tests', () => {
 
@@ -16,10 +17,13 @@ describe('Plugins tests', () => {
             it(`should upload plugin`, (done) => {
                 request(app)
                     .post(`/api/plugins/upload`)
-                    .attach('file', './files/kaholo-plugin.csv.zip')
+                    .attach('file', pluginFile)
                     .expect(200)
                     .then(res => {
-                        expect(res.body.name).toEqual("CSV");
+                        expect(res.body.active).toBe(true);
+                        expect(Array.isArray(res.body.methods)).toBe(true);
+                        expect(Array.isArray(res.body.methods[0].params)).toBe(true);
+                        pluginID = res.body.id;
                         done();
                     });
             });
@@ -27,27 +31,62 @@ describe('Plugins tests', () => {
 
         describe(`GET /`, () => {
             it(`should respond with a list of plugins`, (done) => {
-
+                request(app)
+                    .get(`/api/plugins`)
+                    .expect(200)
+                    .then(res => {
+                        expect(Array.isArray(res.body)).toBe(true);
+                        expect(Array.isArray(res.body[0].methods)).toBe(true);
+                        expect(Array.isArray(res.body[0].methods[0].params)).toBe(true);
+                        done();
+                    });
             });
         });
 
         describe(`GET /:id`, () => {
             it(`should respond with a plugin of a given id`, (done) => {
-
+                request(app)
+                    .get(`/api/plugins/${pluginID}`)
+                    .expect(200)
+                    .then(res => {
+                        expect(Array.isArray(res.body.methods)).toBe(true);
+                        expect(Array.isArray(res.body.methods[0].params)).toBe(true);
+                        done();
+                    });
+                    
             });
         });
 
         // notice: POST method used for "update" operation
         describe(`POST /:id/settings`, () => {
-            it(`should respond with an updated plugin`, (done) => {
-
+            it(`should respond with an updated plugin's settings`, (done) => {
+                const newSettings = [
+                    {
+                        "value": "testvalue3"
+                    },
+                    {
+                        "value": "testvalue4"
+                    }
+                ];
+                request(app)
+                    .post(`/api/plugins/${pluginID}/settings`)
+                    .send(newSettings)
+                    .expect(200)
+                    .then( res=> {
+                        expect(res.body.settings[0].value).toEqual(newSettings[0].value);
+                        expect(res.body.settings[1].value).toEqual(newSettings[1].value);
+                        done();
+                    });
             });
         });
 
         // notice: redundant "/delete"
         describe(`DELETE /:id/delete`, () => {
-            it(`should respond with a plugin of a given id`, (done) => {
-
+            it(`should respond with 200`, (done) => {
+                // notice: RETURNS 200 for ANY ID
+                request(app)
+                    .delete(`/api/plugins/${pluginID}`/delete)
+                    .expect(200, done);
             });
         });
 
@@ -64,6 +103,10 @@ describe('Plugins tests', () => {
         describe(`GET /`, () => {
             it(`should respond with status 500 and proper error msg`, (done) => {
                 // TODO: how to get 500 here?
+            });
+
+            it(`should respond with 'null'`, (done) => {
+
             });
         });
 
