cf3b5829ce650a0e72d6728e2837929aab8e16ff
chbernat <bernat.christopher@gmail.com>
2019-09-12 01:24:48 +0200

    added db name changing

--- server/jest.config.js
+++ server/jest.config.js
@@ -3,12 +3,12 @@ module.exports = {
     coveragePathIgnorePatterns: ["/node_modules/"], // default
     coverageReporters: ["json", "clover"],
     displayName: {
-        name: 'Node.js SERVER',
+        name: 'KAHOLO SERVER',
         color: 'blue',
     },
-    projects: ["./"],
+    projects: ["./tests"],
     collectCoverage: true,
     runner: 'jest-runner',
-    testRunner: 'jasmine2'
+    testRunner: 'jasmine2',
     // coverageThreshold: https://github.com/facebook/jest/blob/master/docs/Configuration.md
 };
