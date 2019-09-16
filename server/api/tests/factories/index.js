a6ca7fc5d4b820f1cbcf60b4d228100f8f51999f
chbernat <bernat.christopher@gmail.com>
2019-09-16 09:56:53 +0200

    Tests, factory, fix for controller

--- server/api/tests/factories/index.js
+++ server/api/tests/factories/index.js
@@ -1,7 +1,9 @@
 const vaultsFactory = require('./vaults.factory');
+const scheduledJobsFactory = require('./scheduled-jobs.factory');
 const TestDataManager = require('./test-data-manager');
 
 module.exports =  {
     TestDataManager,
+    scheduledJobsFactory,
     vaultsFactory
 };
