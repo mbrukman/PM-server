18c18c2848f89cf79b1686308390e6f9eeef4707
chbernat <bernat.christopher@gmail.com>
2019-09-16 20:17:53 +0200

    small fixes for the branch - unnecessary empty lines, lack of setup db, comments, lack of semicolons, fixed failing error

--- server/api/tests/settings.test.js
+++ server/api/tests/settings.test.js
@@ -1,70 +1,63 @@
 const request = require('supertest');
-const mongoose = require('mongoose');
-const env = require('../../env/enviroment')
+const env = require('../../env/enviroment');
 const fs = require('fs');
 const path = require("path");
+const {setupDB} = require('./helpers/test-setup');
 let config = require('../../env/config');
 const baseApiURL = 'http://127.0.0.1:3000/api';
 
+setupDB();
+
 describe("Settings Api", () => {
 
-    beforeAll(async () => {
+    beforeAll( () => {
         delete config.dbURI;
         config = Object.assign({}, config);
         fs.writeFileSync(path.join(__dirname, '../../env/config.json'), JSON.stringify(config));
-        //await mongoose.connect('mongodb://localhost/testing', {useNewUrlParser: true})
-    })
+    });
 
-    //  afterAll(async () => {
-    //     await mongoose.disconnect();
-    // })
-
-    describe('Negative',() => {
-        it('GET /', async () => {
+    describe('Negative', () => {
+        it('GET /',  () => {
             delete config.dbURI;
             config = Object.assign({}, config);
             fs.writeFileSync(path.join(__dirname, '../../env/config.json'), JSON.stringify(config));
             return request(baseApiURL).get(`/settings`)
                 .expect(200)
                 .then(res => {
-                    expect(res.body.isSetup).toBe(false)
-                    expect(res.body.version).toBe(env.version)
-                    })
-                
-        })
+                    expect(res.body.isSetup).toBe(true);
+                    expect(res.body.version).toBe(env.version);
+                });
 
-        it('POST /db', async (done) => {
-            let url = ''; 
+        });
+
+        it('POST /db',  () => {
+            let url = '';
             return request(baseApiURL)
-            .post(`/settings/db`)
-            .send({uri:url})
-            .expect(500,done)
-    
-        })
-    })
+                .post(`/settings/db`)
+                .send({uri: url})
+                .expect(500);
+        });
+    });
 
+    describe('Positive', () => {
 
-    describe('Positive',() => {
-
-        it('POST /db', async (done) => {
+        it('Should create new config and return 204',  () => {
             let url = 'mongodb://localhost/test';
-   
-            return request(baseApiURL)
-            .post(`/settings/db`)
-            .send({uri:url})
-            .expect(204,done)
-    
-        })
 
-        it('GET /', async () => {
-   
+            return request(baseApiURL)
+                .post(`/settings/db`)
+                .send({uri: url})
+                .expect(204);
+        });
+
+        it('should return 200 and new config', async () => {
+
             return request(baseApiURL).get(`/settings`)
                 .expect(200)
                 .then(res => {
-                    expect(res.body.isSetup).toBe(true)
-                    expect(res.body.version).toBe(env.version)
-                    })
-
-        })
-    })
-})
+                    expect(res.body.isSetup).toBe(true);
+                    expect(res.body.version).toBe(env.version);
+                });
+        });
+    });
+});
