f57567120f2cdfae412345c312edec8b9591ad59
chbernat <bernat.christopher@gmail.com>
2019-09-17 15:31:55 +0200

    added factories and tests for map/results

--- server/api/tests/factories/index.js
+++ server/api/tests/factories/index.js
@@ -1,14 +1,16 @@
-const vaultsFactory = require("./vaults.factory");
-const mapStructureFactory = require("./map-structure.factory");
 const scheduledJobsFactory = require("./scheduled-jobs.factory");
+const mapStructureFactory = require("./map-structure.factory");
+const mapResultFactory = require("./map-result.factory");
 const TestDataManager = require("./test-data-manager");
 const projectsFactory = require("./projects.factory");
-const mapsFactory = require("./maps.factory");
 const processFactory = require("./process.factory");
+const vaultsFactory = require("./vaults.factory");
+const mapsFactory = require("./maps.factory");
 
 module.exports = {
   scheduledJobsFactory,
   mapStructureFactory,
+  mapResultFactory,
   projectsFactory,
   TestDataManager,
   processFactory,
