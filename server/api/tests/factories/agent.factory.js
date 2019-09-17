bb40cdad0e93c144e8d64c1d0cc7766d86b9baad
chbernat <bernat.christopher@gmail.com>
2019-09-17 15:39:48 +0200

    agent factory

---
+++ server/api/tests/factories/agent.factory.js
@@ -0,0 +1,52 @@
+const {jsf} = require('./jsf.helper');
+
+
+function generateSingleSchema() {
+    return {
+        type: "object",
+        properties: {
+            name: {
+                type: "string",
+                chance: {
+                    word: {}
+                }
+            },
+            url: {
+                type: "string",
+                chance: {
+                    url: {
+                    }
+                }
+            },
+            publicUrl: {
+                type: "string",
+                chance: {
+                    url: {
+                    }
+                }
+            },
+            key: {
+                type: "boolean",
+                chance: {
+                    guid: {
+                    }
+                }
+            }
+        },
+        required: ['key', 'publicUrl', 'url', 'name']
+    }
+}
+
+function generateMany() {
+    return jsf.generate({
+        type: 'array',
+        items: generateSingleSchema(),
+        maxItems: 15,
+        minItems: 5,
+    })
+}
+
+module.exports = {
+    generateMany,
+    generateOne: generateSingleSchema,
+};
