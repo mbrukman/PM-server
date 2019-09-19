ff95d3aa5e442a84db6a58edc5b8f79324d99be8
David Gahnassia <davidgahnassia@gmail.com>
2019-09-19 16:22:58 +0300

    KAH-14
    Maps - CRUD endpoints tests

--- server/api/tests/factories/map-result.factory.js
+++ server/api/tests/factories/map-result.factory.js
@@ -1,64 +1,75 @@
-const {jsf} = require('./jsf.helper');
-const agentResultsFactory = require('./agent-result.factory');
+const { jsf } = require("./jsf.helper");
+const agentResultsFactory = require("./agent-result.factory");
 
-function generateSingleSchema({mapId, agentId, actionId, processId, mapStructureId}) {
-    const agentsResults = agentResultsFactory.generateMany({agentId, actionId, processId});
+function generateSingleSchema({
+  mapId,
+  agentId,
+  actionId,
+  processId,
+  mapStructureId
+}) {
+  const agentsResults = agentResultsFactory.generateMany({
+    agentId,
+    actionId,
+    processId
+  });
 
-    return {
-        type: 'object',
-        properties: {
-            structure: mapStructureId,
-            status: {
-                type: 'string',
-                enum: [
-                    'running',
-                    'done',
-                    'pending',
-                    'error'
-                ]
-            },
-            reason: {
-                type: 'string',
-                chance: {
-                    word: {
-                        length: 5
-                    }
-                }
-            },
-            trigger: {
-                type: 'string',
-                chance: {
-                    word: {
-                        length: 5
-                    }
-                }
-            },
-            map: mapId,
-            agentsResults,
-            _id: {
-                "type": "string",
-                "format": "mongoID"
-            }
-        },
-        required: ['trigger', 'reason', 'map', 'agentsResults', 'status', 'structure'],
-    };
-
+  return {
+    type: "object",
+    properties: {
+      structure: mapStructureId,
+      status: {
+        type: "string",
+        enum: ["running", "done", "pending", "error"]
+      },
+      reason: {
+        type: "string",
+        chance: {
+          word: {
+            length: 5
+          }
+        }
+      },
+      trigger: {
+        type: "string",
+        chance: {
+          word: {
+            length: 5
+          }
+        }
+      },
+      map: mapId,
+      agentsResults,
+      _id: {
+        type: "string",
+        format: "mongoID"
+      }
+    },
+    required: [
+      "trigger",
+      "reason",
+      "map",
+      "agentsResults",
+      "status",
+      "structure"
+    ]
+  };
 }
 
 function generateMany(idsCollection) {
-    return jsf.generate({
-        type: 'array',
-        items: generateSingleSchema(idsCollection),
-        maxItems: 15,
-        minItems: 5,
-    })
+  return jsf.generate({
+    type: "array",
+    items: generateSingleSchema(idsCollection),
+    maxItems: 15,
+    minItems: 5
+  });
 }
 
 function generateOne(idsCollection) {
-    return jsf.generate(generateSingleSchema(idsCollection));
+  return jsf.generate(generateSingleSchema(idsCollection));
 }
 
 module.exports = {
-    generateOne,
-    generateMany
+  generateOne,
+  generateMany
 };
