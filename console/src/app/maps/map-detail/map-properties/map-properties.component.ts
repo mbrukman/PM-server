fa9b9c2a20d130b1c48678a1d09f8a19ce8f9e64
chbernat <bernat.christopher@gmail.com>
2019-09-20 14:51:40 +0200

    update accordingly to map.model, added better unsubscribe

--- console/src/app/maps/map-detail/map-properties/map-properties.component.ts
+++ console/src/app/maps/map-detail/map-properties/map-properties.component.ts
@@ -1,11 +1,13 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
-import { Subscription } from 'rxjs';
-import { filter, tap, mergeMap } from "rxjs/operators";;
-import { MapsService } from '../../maps.service';
-import { Map } from '@maps/models/map.model';
-import { ProjectsService } from '@projects/projects.service';
-import { Project } from '@projects/models/project.model';
-import { SelectItem } from 'primeng/primeng';
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {Subscription} from 'rxjs';
+import {filter, tap, mergeMap} from "rxjs/operators";
+
+;
+import {MapsService} from '../../maps.service';
+import {Map} from '@maps/models/map.model';
+import {ProjectsService} from '@projects/projects.service';
+import {Project} from '@projects/models/project.model';
+import {SelectItem} from 'primeng/primeng';
 
 
 @Component({
@@ -16,14 +18,15 @@ import { SelectItem } from 'primeng/primeng';
 export class MapPropertiesComponent implements OnInit, OnDestroy {
   map: Map;
   projects: Project[];
-  projectsDropDown:SelectItem[] = [];
+  projectsDropDown: SelectItem[] = [];
   mapSubscription: Subscription;
   selectedProject: string;
   queue: number;
-  onInit:boolean;
-  processesDropDown:SelectItem[] = [];
-  apiResponseAfterProcess:string;
-  apiResponseCodeRefrence:string;
+  onInit: boolean;
+  processesDropDown: SelectItem[] = [];
+  apiResponseAfterProcess: string;
+  apiResponseCodeRefrence: string;
+
   constructor(private mapsService: MapsService, private projectsService: ProjectsService) {
   }
 
@@ -32,50 +35,50 @@ export class MapPropertiesComponent implements OnInit, OnDestroy {
     this.mapSubscription = this.mapsService.getCurrentMap().pipe(
       tap(map => this.map = map),
       filter(map => map),
-      mergeMap(() => this.projectsService.list(null,null,{isArchived:false,globalFilter:null,sort:'-createdAt'}))// filtering empty map result
+      mergeMap(() => this.projectsService.list(null, null, {isArchived: false, globalFilter: null, sort: '-createdAt'}))// filtering empty map result
     ).subscribe(data => {
 
-        if(this.onInit){
-          this.getProcessByMapId();
-          this.apiResponseCodeRefrence = this.map.apiResponseCodeReference;
-        }
+      if (this.onInit) {
+        this.getProcessByMapId();
+        this.apiResponseCodeRefrence = this.map.apiResponseCodeReference;
+      }
 
-        this.projects = data.items;
-        this.projectsDropDown = this.projects.map(project => {
-          return {label:project.name,value:project._id}
-        })
-        
-        let project = this.projects.find((o) => (<string[]>o.maps).indexOf(this.map.id) > -1);
-        if (project && this.onInit) {
-          this.selectedProject = this.map.project ? this.map.project.id : project._id;
-          this.onInit = false;
+      this.projects = data.items;
+      this.projectsDropDown = this.projects.map(project => {
+        return {label: project.name, value: project._id}
+      })
+
+      let project = this.projects.find((o) => (<string[]>o.maps).indexOf(this.map.id) > -1);
+      if (project && this.onInit) {
+        this.selectedProject = this.map.project ? this.map.project.id : project._id;
+        this.onInit = false;
+      }
+    });
+  }
+
+  getProcessByMapId() {
+    const currentMapSub = this.mapsService.getCurrentMapStructure().pipe(filter((structure) => !!structure))
+      .subscribe(structure => {
+        if (structure.processes.length) {
+          this.apiResponseAfterProcess = this.map.processResponse;
+          this.onChangeProcessResponse();
+          this.processesDropDown.push({label: 'Select a Process', value: null});
+          let processes = structure.processes.map(process => {
+            return {label: process.name || process.used_plugin.name, value: process.uuid}
+          })
+          this.processesDropDown.push(...processes);
         }
       });
+    this.mapSubscription.add(currentMapSub)
   }
 
-  getProcessByMapId(){
-    this.mapsService.getCurrentMapStructure().pipe(filter((structure) => !!structure))
-    .subscribe(structure => {
-      if(structure.processes.length){
-        this.apiResponseAfterProcess = this.map.processResponse;
-        this.onChangeProcessResponse();
-        this.processesDropDown.push({label:'Select a Process',value:null});
-        let processes = structure.processes.map(process => {
-          return {label:process.name || process.used_plugin.name, value:process.uuid}
-        })
-        this.processesDropDown.push(...processes);
-
-      }
-    })
-  }
-
-  onChangeProcessResponse(){
-    let mapObj = Object.assign({}, this.map, { processResponse: this.apiResponseAfterProcess });
+  onChangeProcessResponse() {
+    let mapObj = Object.assign({}, this.map, {processResponse: this.apiResponseAfterProcess});
     this.mapsService.setCurrentMap(mapObj);
   }
 
-  onapiResponseCodeReference(){
-    let mapObj = Object.assign({}, this.map, { apiResponseCodeReference: this.apiResponseCodeRefrence });
+  onapiResponseCodeReference() {
+    let mapObj = Object.assign({}, this.map, {apiResponseCodeReference: this.apiResponseCodeRefrence});
     this.mapsService.setCurrentMap(mapObj);
   }
 
@@ -88,15 +91,16 @@ export class MapPropertiesComponent implements OnInit, OnDestroy {
   }
 
   onChangeProject() {
-    let mapObj = Object.assign({}, this.map, { project: this.selectedProject});
+    let mapObj = Object.assign({}, this.map, {project: {id: this.selectedProject}});
     this.mapsService.setCurrentMap(mapObj);
   }
 
-  archiveMap(isArchive:boolean) {
-    this.mapsService.archive(this.map.id, isArchive).subscribe(() => {
+  archiveMap(isArchive: boolean) {
+    const archiveSub = this.mapsService.archive(this.map.id, isArchive).subscribe(() => {
       this.map.archived = isArchive;
       this.mapsService.setCurrentMap(this.map);
     });
+    this.mapSubscription.add(archiveSub);
   }
 
 }
