e01a6771443d9a39c98fef135c1cc972d2c50d40
chbernat <bernat.christopher@gmail.com>
2019-09-24 18:36:40 +0200

    added helper and test

--- server/api/tests/helpers/index.js
+++ server/api/tests/helpers/index.js
@@ -1,5 +1,41 @@
-module.exports = {
-    randomIdx(length) {
-        return Math.floor(Math.random() * length);
-    }
+const socket = require('socket.io-client');
+const ProjectModel = require('../../../api/models/project.model');
+const TestDataManager = require('./../factories/test-data-manager');
+const {MapStructure} = require('../../../api/models/map-structure.model');
+const {mapStructureFactory, mapsFactory, projectsFactory} = require('./../factories');
+
+function randomIdx(length) {
+    return Math.floor(Math.random() * length);
 }
+
+module.exports = {
+    randomIdx,
+    connectToSocket(url = 'http://localhost:3000/') {
+        const io = socket(url);
+        return new Promise((resolve, reject) => {
+            io.on('connect', () => {
+                resolve(io);
+            });
+        });
+    },
+    async generateMapAndProject() {
+        const mapStructureTestDataManager = new TestDataManager(MapStructure);
+        const projectTestDataManager = new TestDataManager(ProjectModel);
+        const projects = await projectTestDataManager.generateInitialCollection(
+            projectsFactory.generateProjects()
+        );
+
+        const randomIndex = randomIdx(projectTestDataManager.collection.length);
+        const project = projectTestDataManager.collection[randomIndex];
+
+        const map = await mapsFactory.createMap(project.id, 'random map name');
+        const mapStructures = await mapStructureTestDataManager.generateInitialCollection(
+            mapStructureFactory.generateMany(map._id.toString(), [map])
+        );
+        return {
+            map,
+            projects,
+            mapStructures
+        }
+    }
+};
