8bd2807d37f4453a9adb24b5a89cfce835cd45ed
David Gahnassia <davidgahnassia@gmail.com>
2019-09-25 12:43:00 +0300

    format

--- server/api/tests/agents.test.js
+++ server/api/tests/agents.test.js
@@ -39,10 +39,10 @@ describe('Agents tests', () => {
                 return request(apiURL)
                     .get(`/agents`)
                     .expect(200)
-                    .then(({body}) => {
+                    .then(({ body }) => {
                         expect(body[randomIndex].id).toEqual(agentId)
                     })
-                })
+            })
         });
 
         describe(`GET /groups`, () => {
@@ -52,25 +52,25 @@ describe('Agents tests', () => {
                 return request(apiURL)
                     .get(`/agents/groups`)
                     .expect(200)
-                    .then(({body}) => {
+                    .then(({ body }) => {
                         expect(body[randomIndex].id).toEqual(groupId)
                     })
-                })
+            })
         });
 
         describe(`POST /groups`, () => {
             it(`should respond with the groups`, () => {
                 const randomIndex = randomIdx(groupsTestDataManager.collection.length);
-                const {id, name} = groupsTestDataManager.collection[randomIndex];
+                const { id, name } = groupsTestDataManager.collection[randomIndex];
                 return request(apiURL)
                     .post(`/agents/groups`)
-                    .send({name:name, _id:id})
+                    .send({ name: name, _id: id })
                     .expect(200)
-                    .then(({body}) => {
+                    .then(({ body }) => {
                         expect(body[0].id).toEqual(id)
                         expect(body[0].name).toEqual(name)
                     })
-                })
+            })
         });
 
         describe(`PUT /groups/:id/add-agent`, () => {
@@ -82,10 +82,10 @@ describe('Agents tests', () => {
                     .put(`/agents/groups/${groupId}/add-agent`)
                     .send([agent._id])
                     .expect(200)
-                    .then(({body}) => {
+                    .then(({ body }) => {
                         expect(body.agents[body.agents.length - 1]).toEqual(agent._id)
                     })
-                })
+            })
         });
 
         describe(`PUT /groups/:id`, () => {
@@ -95,12 +95,12 @@ describe('Agents tests', () => {
                 const groupName = 'random group name';
                 return request(apiURL)
                     .put(`/agents/groups/${groupId}`)
-                    .send({name:groupName})
+                    .send({ name: groupName })
                     .expect(200)
-                    .then(({body}) => {
+                    .then(({ body }) => {
                         expect(body.name).toEqual(groupName)
                     })
-                })
+            })
         });
 
         describe(`POST /groups/:id/add-filters`, () => {
@@ -118,10 +118,10 @@ describe('Agents tests', () => {
                     .post(`/agents/groups/${groupId}/add-filters`)
                     .send(newFilter)
                     .expect(200)
-                    .then(({body}) => {
+                    .then(({ body }) => {
                         expect(body.filters[0]).toMatchObject(newFilter[0])
                     })
-                })
+            })
         });
 
         describe(`POST /groups/create`, () => {
@@ -131,11 +131,11 @@ describe('Agents tests', () => {
                     .post(`/agents/groups/create`)
                     .send(newGroup)
                     .expect(200)
-                    .then(({body}) => {
+                    .then(({ body }) => {
                         expect(body.id).toEqual(newGroup._id)
                         expect(body.name).toEqual(newGroup.name);
                     })
-                })
+            })
         });
 
         describe(`GET /groups/:id`, () => {
@@ -145,10 +145,10 @@ describe('Agents tests', () => {
                 return request(apiURL)
                     .get(`/agents/groups/${groupId}`)
                     .expect(200)
-                    .then(({body}) => {
+                    .then(({ body }) => {
                         expect(body.id).toEqual(groupId)
                     })
-                })
+            })
         });
 
         describe(`DELETE /groups/:id`, () => {
@@ -158,41 +158,41 @@ describe('Agents tests', () => {
                 return request(apiURL)
                     .delete(`/agents/groups/${groupId}`)
                     .expect(200)
-                    .then(({text}) => {
+                    .then(({ text }) => {
                         expect(text).toEqual(groupId)
                     })
-                })
+            })
         });
 
         describe(`POST /groups/:id/remove-agent`, () => {
             it(`should respond with the group`, () => {
                 const groupRandomIndex = randomIdx(groupsTestDataManager.collection.length);
-                const {id, agents} = groupsTestDataManager.collection[groupRandomIndex];
+                const { id, agents } = groupsTestDataManager.collection[groupRandomIndex];
                 const agent = agents[0];
                 return request(apiURL)
                     .post(`/agents/groups/${id}/remove-agent`)
-                    .send({agentId:agent})
+                    .send({ agentId: agent })
                     .expect(200)
-                    .then(({body}) => {
+                    .then(({ body }) => {
                         expect(body.id).toEqual(id)
                         expect(body.agents.includes(agent)).toEqual(false)
                     })
-                })
+            })
         });
 
         describe(`DELETE /groups/:id/filters/:index`, () => {
             it(`should respond with the group`, () => {
                 const groupRandomIndex = randomIdx(groupsTestDataManager.collection.length);
-                const {id, filters} = groupsTestDataManager.collection[groupRandomIndex];
+                const { id, filters } = groupsTestDataManager.collection[groupRandomIndex];
                 const filterRandomIndex = randomIdx(filters.length)
                 return request(apiURL)
                     .delete(`/agents/groups/${id}/filters/${filterRandomIndex}`)
                     .expect(200)
-                    .then(({body}) => {
-                       expect(body.id).toEqual(id);
-                       expect(body.filters.length).toEqual(filters.length -1)
+                    .then(({ body }) => {
+                        expect(body.id).toEqual(id);
+                        expect(body.filters.length).toEqual(filters.length - 1)
                     })
-                })
+            })
         });
 
         describe(`DELETE /:id`, () => {
@@ -202,10 +202,10 @@ describe('Agents tests', () => {
                 return request(apiURL)
                     .delete(`/agents/${agentId}`)
                     .expect(200)
-                    .then(({text}) => {
-                       expect(text).toEqual('OK')
+                    .then(({ text }) => {
+                        expect(text).toEqual('OK')
                     })
-                })
+            })
         });
 
         describe(`PUT /:id`, () => {
@@ -215,16 +215,16 @@ describe('Agents tests', () => {
                 const newAgentName = 'random agent name';
                 return request(apiURL)
                     .put(`/agents/${agentId}`)
-                    .send({name:newAgentName})
+                    .send({ name: newAgentName })
                     .expect(200)
-                    .then(({body}) => {
-                       expect(body.id).toEqual(agentId);
-                       expect(body.name).toEqual(newAgentName)
+                    .then(({ body }) => {
+                        expect(body.id).toEqual(agentId);
+                        expect(body.name).toEqual(newAgentName)
                     })
-                })
+            })
         });
     });
-    
+
 
     describe('Negative', () => {
 
@@ -232,16 +232,16 @@ describe('Agents tests', () => {
             it(`should respond with 500 status code`, (done) => {
                 return request(apiURL)
                     .put(`/agents/groups/0/add-agent`)
-                    .expect(500,done)
-                })
+                    .expect(500, done)
+            })
         });
 
         describe(`PUT /groups/:id`, () => {
             it(`should respond with 500 status code`, (done) => {
                 return request(apiURL)
                     .put(`/agents/groups/0`)
-                    .expect(500,done)
-                })
+                    .expect(500, done)
+            })
         });
 
         describe(`POST /groups/:id/add-filters`, () => {
@@ -256,64 +256,64 @@ describe('Agents tests', () => {
                 return request(apiURL)
                     .post(`/agents/groups/0/add-filters`)
                     .send(newFilter)
-                    .expect(500,done)
-                })
+                    .expect(500, done)
+            })
         });
 
         describe(`POST /groups/create`, () => {
             it(`should respond with 500 status code`, (done) => {
                 return request(apiURL)
                     .post(`/agents/groups/create`)
-                    .expect(500,done)
-                })
+                    .expect(500, done)
+            })
         });
 
         describe(`GET /groups/:id`, () => {
             it(`should respond with 500 status code`, (done) => {
                 return request(apiURL)
                     .get(`/agents/groups/0`)
-                    .expect(500,done)
-                })
+                    .expect(500, done)
+            })
         });
 
         describe(`DELETE /groups/:id`, () => {
             it(`should respond with 500 status code`, (done) => {
                 return request(apiURL)
                     .delete(`/agents/groups/0`)
-                    .expect(500,done)
-                })
+                    .expect(500, done)
+            })
         });
 
         describe(`POST /groups/:id/remove-agent`, () => {
             it(`should respond with 500 status code`, (done) => {
                 return request(apiURL)
                     .post(`/agents/groups/0/remove-agent`)
-                    .expect(500,done)
-                })
+                    .expect(500, done)
+            })
         });
 
         describe(`DELETE /groups/:id/filters/:index`, () => {
             it(`should respond with 500 status code`, (done) => {
                 return request(apiURL)
                     .delete(`/agents/groups/0/filters/0`)
-                    .expect(500,done)
-                })
+                    .expect(500, done)
+            })
         });
 
         describe(`DELETE /:id`, () => {
             it(`should respond with 500 status code`, (done) => {
                 return request(apiURL)
                     .delete(`/agents/0`)
-                    .expect(500,done)
-                })
+                    .expect(500, done)
+            })
         });
 
         describe(`PUT /:id`, () => {
             it(`should respond with 500 status code`, (done) => {
                 return request(apiURL)
                     .put(`/agents/0`)
-                    .expect(500,done)
-                })
+                    .expect(500, done)
+            })
         });
     });
 });
