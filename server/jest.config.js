60baa323fd8abaf97213ec6a773b577d8bcefa50
chbernat <bernat.christopher@gmail.com>
2019-09-25 10:50:58 +0200

    upgraded the ws bootstrap

--- server/jest.config.js
+++ server/jest.config.js
@@ -13,6 +13,9 @@ module.exports = {
     forceExit: true,
     runner: 'jest-runner',
     testRunner: "jest-circus/runner",
-    setupFilesAfterEnv: ['./api/tests/helpers/global-test-setup.js']
+    setupFilesAfterEnv: ['./api/tests/helpers/global-test-setup.js'],
+    globals: {
+        websocketURL: 'http://localhost:3000/'
+    }
     // coverageThreshold: https://github.com/facebook/jest/blob/master/docs/Configuration.md
 };
