78ae83cd5f7bf955d7de902006016cf1ebdd22d0
arispen-desktop <arispen@gmail.com>
2019-09-26 16:32:25 +0200

    config manipulation WIP

--- server/api/tests/settings.test.js
+++ server/api/tests/settings.test.js
@@ -1,10 +1,25 @@
 const request = require('supertest');
+const fs = require('fs');
+const path = require('path');
+const _ = require("lodash");
+
 const app = 'localhost:3000';
+let originalConfig = {};
+const testConfig = { "upload_path": "uploads", "interval_time": 5000, "retries": 3, "page_size": 15, "dbURI": "mongodb+srv://admin:7vXYF4nNu8B9SnH5@cluster0-qnjcz.mongodb.net/jest-settings-test", "serverKey": "b92a5c991d68e544a3fa21da9b083903" }
 
 describe('Settings tests', () => {
+
+    beforeAll(() => {
+        // copy original env/config.json so the tests won't override it
+        originalConfig = require('../../env/config');
+    });
+
+    afterAll(() => {
+        // recreate the original config
+        fs.writeFileSync(path.join(__dirname, '../../env/config.json'), JSON.stringify(originalConfig));
+    });
+
     describe('GET api/settings', () => {
-        it.todo(`should handle lack of setup of dbURI`);
-        // change config?
 
         it(`should respond with 'true' for valid db connection`, () => {
             return request(app)
@@ -17,8 +32,47 @@ describe('Settings tests', () => {
                 });
         });
 
-        it.todo(`should respond with 'false' for invalid db connection`);
-        // disconnect with db?
+        describe('in case of no db uri', () => {
+            beforeAll(() => {
+                delete testConfig.dbURI;
+                fs.writeFileSync(path.join(__dirname, '../../env/config.json'), JSON.stringify(testConfig));
+
+                delete require.cache[Object.keys(require.cache).filter(i => i.includes('env/config'))[0]];
+                // TODO: both logs should return the same value
+                console.warn(require('../../env/config'));
+                console.warn(fs.readFileSync(path.join(__dirname, '../../env/config.json'), 'UTF-8'));
+            });
+
+            it.only(`should handle lack of setup of dbURI`, () => {
+
+                return request(app)
+                    .get('/api/settings')
+                    .expect(200)
+                    .then(({ body }) => {
+                        expect(body.isSetup).toBe(false);
+                        expect(body.version).toBeDefined();
+                        expect(typeof body.version).toBe('string');
+                    });
+            });
+        });
+
+        describe('in case of invalid db uri', () => {
+            beforeAll(() => {
+                testConfig.dbURI = "wrong-uri";
+                fs.writeFileSync(path.join(__dirname, '../../env/config.json'), JSON.stringify(testConfig));
+            });
+
+            it(`should respond with 'false' for invalid db connection`, () => {
+                return request(app)
+                    .get('/api/settings')
+                    .expect(200)
+                    .then(({ body }) => {
+                        expect(body.isSetup).toBe(false);
+                        expect(body.version).toBeDefined();
+                        expect(typeof body.version).toBe('string');
+                    });
+            });
+        });
     });
 
     describe('POST api/settings/db', () => {
