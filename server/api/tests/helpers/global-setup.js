d8c873e1ef87a908f9e79b8fa7a4dab0b2dc8951
chbernat <bernat.christopher@gmail.com>
2019-09-26 14:00:41 +0200

    test config generation

--- server/api/tests/helpers/global-setup.js
+++ server/api/tests/helpers/global-setup.js
@@ -1,7 +1,21 @@
 const { app, server, port, winston } = require("../../index");
 const bootstrap = require("../../../helpers/bootstrap").bootstrap;
+const { envDir } = require("../../../env");
+const fs = require("fs");
+const { MongoMemoryServer } = require("mongodb-memory-server");
 
 module.exports = async () => {
+  const mongoMemServer = new MongoMemoryServer();
+  const connectionString = await mongoMemServer.getConnectionString();
+  const testConfigPath = `${envDir}/config.test.json`;
+  const prodConfigPath = `${envDir}/config.json`;
+
+  const prodConfig = fs.readFileSync(prodConfigPath);
+
+  prodConfig.dbURI = connectionString;
+
+  fs.writeFileSync(testConfigPath, JSON.stringify(prodConfig));
+
   global.server = server.listen(port, () => {
     winston.log("info", `Running on localhost:${port}`);
     bootstrap(app);
--- server/env/enviroment.js
+++ server/env/enviroment.js
@@ -1,22 +1,32 @@
-const path = require('path');
-const config = require('./config');
-const packageJson = require('../package.json');
-const fs = require('fs');
+const path = require("path");
+const prodConfig = require("./config");
+const testConfig = require("./config.test");
+const packageJson = require("../package.json");
+const fs = require("fs");
 
 // //////////////////////////
 //   Key creation
 // //////////////////////////
 
+let config = prodConfig;
+
+if (process.env.NODE_ENV === "test") {
+  config = testConfig;
+}
+
 if (!config.serverKey) {
-  const createKey = require('../helpers/create-key');
+  const createKey = require("../helpers/create-key");
   config.serverKey = createKey.generateKey();
-  fs.writeFileSync('./config.json', JSON.stringify(config), 'utf8'); // write it back
+  fs.writeFileSync("./config.json", JSON.stringify(config), "utf8"); // write it back
 }
 // //////////////////////////
 
 const version = packageJson.version;
-const keyPath = {keyPath: path.join(__dirname, '.khkey')};
-const static_cdn = {static_cdn: path.join(path.dirname(path.dirname(__dirname)), 'static_cdn')};
-const env = Object.assign({}, static_cdn, keyPath, config, {version});
+const keyPath = { keyPath: path.join(__dirname, ".khkey") };
+// eslint-disable-next-line camelcase
+const static_cdn = {
+  static_cdn: path.join(path.dirname(path.dirname(__dirname)), "static_cdn")
+};
+const env = Object.assign({}, static_cdn, keyPath, config, { version });
 
 module.exports = env;
