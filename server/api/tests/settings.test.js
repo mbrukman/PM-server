9c7ecd1330bc8b47f19f3d5bbc4617df0e6fad8b
chbernat <bernat.christopher@gmail.com>
2019-09-30 10:22:19 +0200

    fixes for comments

--- server/api/tests/settings.test.js
+++ server/api/tests/settings.test.js
@@ -3,14 +3,24 @@ const fs = require('fs');
 const path = require('path');
 
 const app = 'localhost:3000';
-let originalConfig = {};
-const testConfig = { "upload_path": "uploads", "interval_time": 5000, "retries": 3, "page_size": 15, "dbURI": "mongodb+srv://admin:7vXYF4nNu8B9SnH5@cluster0-qnjcz.mongodb.net/jest-settings-test", "serverKey": "b92a5c991d68e544a3fa21da9b083903" }
-
 describe('Settings tests', () => {
+    let originalConfig = {};
+
+    const wrongDBURI = 'mongodb://testUser:wrongOneOne@127.0.0.1:27017/test';
+    const testConfig = {
+        "upload_path": "uploads",
+        "interval_time": 5000,
+        "retries": 3,
+        "page_size": 15,
+        "serverKey": "b92a5c991d68e544a3fa21da9b083903"
+    };
+
+
 
     beforeAll(() => {
         // copy original env/config.json so the tests won't override it
         originalConfig = require('../../env/config');
+        testConfig.dbURI = originalConfig;
     });
 
     afterAll(() => {
@@ -23,7 +33,7 @@ describe('Settings tests', () => {
             return request(app)
                 .get('/api/settings')
                 .expect(200)
-                .then(({ body }) => {
+                .then(({body}) => {
                     expect(body.isSetup).toBe(true);
                     expect(body.version).toBeDefined();
                     expect(typeof body.version).toBe('string');
@@ -40,7 +50,7 @@ describe('Settings tests', () => {
                 return request(app)
                     .get('/api/settings')
                     .expect(200)
-                    .then(({ body }) => {
+                    .then(({body}) => {
                         expect(body.isSetup).toBe(false);
                         expect(body.version).toBeDefined();
                         expect(typeof body.version).toBe('string');
@@ -58,7 +68,7 @@ describe('Settings tests', () => {
                 return request(app)
                     .get('/api/settings')
                     .expect(200)
-                    .then(({ body }) => {
+                    .then(({body}) => {
                         expect(body.isSetup).toBe(false);
                         expect(body.version).toBeDefined();
                         expect(typeof body.version).toBe('string');
@@ -71,7 +81,7 @@ describe('Settings tests', () => {
         it(`should respond with 204 after successful connection to given db uri`, () => {
             return request(app)
                 .post('/api/settings/db')
-                .send({ uri: 'mongodb+srv://admin:7vXYF4nNu8B9SnH5@cluster0-qnjcz.mongodb.net/jest-settings-test' })
+                .send({uri: originalConfig.dbURI})
                 .expect(204);
         });
 
@@ -80,7 +90,7 @@ describe('Settings tests', () => {
                 .post('/api/settings/db')
                 .send({})
                 .expect(500)
-                .then(({ text }) => {
+                .then(({text}) => {
                     expect(text).toEqual('Missing parameters');
                 });
         });
@@ -88,7 +98,7 @@ describe('Settings tests', () => {
         it(`should respond with 500 for error connecting to db`, () => {
             return request(app)
                 .post('/api/settings/db')
-                .send({ uri: 'mongodb+srv://admin:wrong-password@cluster0-qnjcz.mongodb.net/jest-settings-test' })
+                .send({uri: wrongDBURI})
                 .expect(500);
         });
     });
