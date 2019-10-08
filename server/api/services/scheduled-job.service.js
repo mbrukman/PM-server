58512a1900785354893f79cc6d6eca426d81eade
chbernat <bernat.christopher@gmail.com>
2019-10-08 15:25:55 +0200

    removed global variable jobs and moved socket variable into new, singleton service

--- server/api/services/scheduled-job.service.js
+++ server/api/services/scheduled-job.service.js
@@ -1,97 +1,110 @@
-const scheduler = require('node-schedule');
-const mapsService = require('../services/maps.service');
-const mapsExecutionService = require('../services/map-execution.service');
-const ScheduledJob = require('../models').ScheduledJob;
-const winston = require('winston');
-const jobs = {};
-let socket;
+const { scheduleJob, Job } = require("node-schedule");
+const mapsExecutionService = require("../services/map-execution.service");
+const ScheduledJob = require("../models").ScheduledJob;
+const winston = require("winston");
 
-module.exports = {
+class ScheduledJobService {
+  constructor() {
+    this.socket = null;
+  }
   /*
-     * adding scheduled job
-     * we are using node-scheduler for this.
-     * every job is saved in the jobs object, which will allow us to conveniently cancel it (see remove jobs);
-     * */
-  addScheduledJob: (job) => {
-    jobs[job._id] = scheduler.scheduleJob((job.datetime || job.cron),
-        function() {
-          ScheduledJob.findById(job._id)
-              .then((jobObj) => {
-                if (!jobObj) {
-                  return;
-                }
-                if (jobObj.skip) {
-                  ScheduledJob.findByIdAndUpdate(job._id, {$set: {skip: false}}).then(() => {
-                    console.log('Removed skip from job');
-                  });
-                  return;
-                }
-                const configuration = {
-                  config: job.configuration,
-                };
-                mapsExecutionService.execute(job.map, null, socket, configuration, 'Started by schedules task')
-                    .then((res) => {
-                      console.log(res);
-                    })
-                    .catch((err)=>{
-                      winston.log('error', err);
-                    });
-              });
-        });
-  },
+   * adding scheduled job
+   * we are using node-scheduler for this.
+   * */
+  addScheduledJob(job) {
+    scheduleJob(job.datetime || job.cron, function() {
+      ScheduledJob.findById(job._id).then(jobObj => {
+        if (!jobObj) {
+          return;
+        }
+        if (jobObj.skip) {
+          ScheduledJob.findByIdAndUpdate(job._id, {
+            $set: { skip: false }
+          }).then(() => {
+            console.log("Removed skip from job");
+          });
+          return;
+        }
+        const configuration = {
+          config: job.configuration
+        };
+        mapsExecutionService
+          .execute(
+            job.map,
+            null,
+            this.socket,
+            configuration,
+            "Started by schedules task"
+          )
+          .then(res => {
+            console.log(res);
+          })
+          .catch(err => {
+            winston.log("error", err);
+          });
+      });
+    });
+  }
   /* creating new job */
-  create: (job) => {
-    return ScheduledJob.create(job).then((jobObj) => {
-      module.exports.addScheduledJob(jobObj);
-      return module.exports.filter({_id: jobObj._id}, true);
+  create(job) {
+    return ScheduledJob.create(job).then(jobObj => {
+      this.addScheduledJob(jobObj);
+      return this.filter({ _id: jobObj._id }, true);
     });
-  },
+  }
   /* deleting job object from db and removing scheduled job if exists */
-  delete: (jobId) => {
-    module.exports.removeScheduledJob(jobId);
+  async delete(jobId) {
+    await this.removeScheduledJob(jobId);
 
-    return ScheduledJob.remove({_id: jobId});
-  },
-  filter: (query = {}, one = false) => {
-    return one ? ScheduledJob.findOne(query).populate('map') : ScheduledJob.find(query).populate('map');
-  },
+    return ScheduledJob.remove({ _id: jobId });
+  }
+  filter(query = {}, one = false) {
+    return one
+      ? ScheduledJob.findOne(query).populate("map")
+      : ScheduledJob.find(query).populate("map");
+  }
   /* return jobs that should happen in the future */
-  getFutureJobs: () => {
+  getFutureJobs() {
     return ScheduledJob.find().where({
-      $or: [
-        {datetime: {$gt: new Date()}},
-        {cron: {$exists: true}},
-      ],
+      $or: [{ datetime: { $gt: new Date() } }, { cron: { $exists: true } }]
     });
-  },
+  }
   /* updating job in db and in scheduled jobs */
-  update: (job) => {
-    return ScheduledJob.findByIdAndUpdate(job._id, job, {new: true}).then((jobObj) => {
-      module.exports.removeScheduledJob(job._id);
-      module.exports.addScheduledJob(jobObj);
-      return jobObj;
-    });
-  },
+  update(job) {
+    return ScheduledJob.findByIdAndUpdate(job._id, job, { new: true })
+      .then(jobObj => {
+        this.removeScheduledJob(job._id);
+        return jobObj;
+      })
+      .then(jobObj => {
+        this.addScheduledJob(jobObj);
+        return jobObj;
+      });
+  }
+
   /* removing a scheduled job */
-  removeScheduledJob: (jobId) => {
-    if (!jobs[jobId]) {
-      return;
-    }
-    jobs[jobId].cancel();
-    delete jobs[jobId];
-  },
+  removeScheduledJob(jobId) {
+    const scheduledJob = ScheduledJob.findById(jobId);
+    const job = new Job(scheduledJob);
+    job.cancel();
+  }
 
   /*
      loading scheduled jobs and set the socket
      * this function is called on bootstrap
      * */
-  loadJobs: (app) => {
-    socket = app.io;
-    module.exports.getFutureJobs().then((jobs) => {
-      jobs.forEach(function(job) {
-        module.exports.addScheduledJob(job);
-      });
-    }).catch((error) => {
-    });
-  },
-};
+  loadJobs(app) {
+    this.socket = app.io;
+    this.getFutureJobs()
+      .then(jobs => {
+        jobs.forEach(function(job) {
+          this.addScheduledJob(job);
+        });
+      })
+      .catch(error => {});
+  }
+}
+
+const scheuledJobService = new ScheduledJobService();
+
+module.exports = scheuledJobService;
