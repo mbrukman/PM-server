0a9390e09ff9bf0a9adba9572e744155dedff4a9
chbernat <bernat.christopher@gmail.com>
2019-10-10 16:59:13 +0200

    commented out or instances of using mongo in memory db and ran tests for 25 times - it works.

--- server/api/tests/helpers/global-setup.js
+++ server/api/tests/helpers/global-setup.js
@@ -1,26 +1,23 @@
 const bootstrap = require("../../../helpers/bootstrap").bootstrap;
-const { envDir } = require("../../../env");
-const fs = require("fs");
-const { MongoMemoryServer } = require("mongodb-memory-server");
+// const { envDir } = require("../../../env");
+// const fs = require("fs");
+// const { MongoMemoryServer } = require("mongodb-memory-server");
 module.exports = async () => {
-  const mongoMemServer = new MongoMemoryServer({});
+  // const prodConfigPath = `${envDir}/config.json`;
+  // const testConfigPath = `${envDir}/config.test.json`;
+  // const prodConfig = JSON.parse(fs.readFileSync(prodConfigPath, "utf-8"));
 
-  const connectionString = await mongoMemServer.getConnectionString();
+  // DO NOT DELETE
+  // const mongoMemServer = new MongoMemoryServer({});
+  // const connectionString = await mongoMemServer.getConnectionString();
+  // global.mongoMemServer = mongoMemServer;
+  // prodConfig.dbURI = connectionString;
+  // fs.writeFileSync(testConfigPath, JSON.stringify(prodConfig));
 
-  global.mongoMemServer = mongoMemServer;
-
-  const prodConfigPath = `${envDir}/config.json`;
-  const testConfigPath = `${envDir}/config.test.json`;
-  const prodConfig = JSON.parse(fs.readFileSync(prodConfigPath, "utf-8"));
-
-  prodConfig.dbURI = connectionString;
-
-  fs.writeFileSync(testConfigPath, JSON.stringify(prodConfig));
-
-  delete require.cache[testConfigPath];
-  delete require.cache[
-    Object.keys(require.cache).filter(i => i.includes("env/enviroment"))[0]
-  ];
+  // delete require.cache[testConfigPath];
+  // delete require.cache[
+  //   Object.keys(require.cache).filter(i => i.includes("env/enviroment"))[0]
+  // ];
 
   const { app, server, port, winston } = require("../../index");
 
