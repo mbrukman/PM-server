c52f973dd3b3affe30f070827964008469442073
arispen <arispen@gmail.com>
2019-10-17 17:04:35 +0200

    agent status WiP

--- server/api/models/agent.model.js
+++ server/api/models/agent.model.js
@@ -1,24 +1,45 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const agentSchema = new Schema({
   name: String,
-  url: {type: String, required: true},
-  publicUrl: {type: String, required: true},
-  key: {type: String, required: true},
+  url: { type: String, required: true },
+  publicUrl: { type: String, required: true },
+  key: { type: String, required: true },
   sshKey: String,
   attributes: [],
-  isDeleted: Boolean,
+  isDeleted: Boolean
+  //status: agentStatusSchema
 });
 
+const agentStatusSchema = new Schema({
+  attributes: [],
+  _id: Schema.Types.ObjectId,
+  name: String,
+  url: String,
+  publicUrl: String,
+  __v: Number,
+  id: String,
+  alive: Boolean,
+  following: Boolean,
+  defaultUrl: String,
+  socketId: String,
+  hostname: String,
+  arch: String,
+  freeSpace: String,
+  respTime: Number,
+  key: String,
+  installed_plugins: Schema.Types.Mixed,
+  liveCounter: Number
+});
 
-agentSchema.set('toJSON', {
+agentSchema.set("toJSON", {
   transform: function(doc, ret, options) {
     ret.id = ret._id;
     delete ret.key;
-  },
+  }
 });
 
-const Agent = mongoose.model('Agent', agentSchema, 'agents');
+const Agent = mongoose.model("Agent", agentSchema, "agents");
 
 module.exports = Agent;
