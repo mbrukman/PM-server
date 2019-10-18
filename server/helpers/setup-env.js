059bae1fbd21dd325ea4ff98c4a4e8bbf027f365
Chris Bernat <bernat.christopher@gmail.com>
2019-10-18 11:58:41 +0200

    restored dirname

--- server/helpers/setup-env.js
+++ server/helpers/setup-env.js
@@ -8,5 +8,5 @@ const packageJson = require("../package.json");
 global.kaholo = {
     VERSION: packageJson.version,
     KEY_PATH: path.join(__dirname, ".khkey"),
-    STATIC_CDN: path.join(path.dirname(__dirname), "static_cdn")
+    STATIC_CDN: path.join(path.dirname(path.dirname(__dirname)), "static_cdn")
 };
