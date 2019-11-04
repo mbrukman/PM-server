2eaf1333cf639f5e4fba69842f6ae3ac771c2a5a
Chris Bernat <bernat.christopher@gmail.com>
2019-11-04 15:24:25 +0100

    improved rxjs flow and filter-options model styling

--- console/src/app/shared/model/filter-options.model.ts
+++ console/src/app/shared/model/filter-options.model.ts
@@ -1,8 +1,8 @@
-export class FilterOptions{
-    globalFilter?: string ;
-    isArchived?:boolean;
-    sort:string = '-createdAt';
-    limit? : number;
-    filter?:any;
-    page?:number = 1 
+export class FilterOptions {
+  globalFilter?: string;
+  isArchived?: boolean;
+  sort: string = '-createdAt';
+  limit?: number;
+  filter?: any;
+  page?: number = 1;
 }
--- console/src/app/users-management/user-group/user-group-list/user-group-list.component.ts
+++ console/src/app/users-management/user-group/user-group-list/user-group-list.component.ts
@@ -1,13 +1,13 @@
-import { Component, OnInit, ElementRef, ViewChild, OnDestroy } from '@angular/core';
-import { Subscription, fromEvent } from 'rxjs';
-import { ActivatedRoute, Data } from '@angular/router';
-import { debounceTime, switchMap } from 'rxjs/operators';
-import { FilterOptions } from '@app/shared/model/filter-options.model';
+import {Component, OnInit, ElementRef, ViewChild, OnDestroy} from '@angular/core';
+import {Subscription, fromEvent} from 'rxjs';
+import {ActivatedRoute, Data} from '@angular/router';
+import {debounceTime, switchMap, tap} from 'rxjs/operators';
+import {FilterOptions} from '@app/shared/model/filter-options.model';
 import UserGroup from '@app/services/user-group/user-group.model';
 import {UserGroupCreateModalComponent} from '@app/users-management/user-group/user-group-list/user-group-create-modal/user-group-create-modal.component';
 import UserGroupDataInterface from '@app/services/user-group/user-group-data.interface';
-import { UserGroupService } from '@app/services/user-group/user-group.service';
-import { BsModalService } from 'ngx-bootstrap';
+import {UserGroupService} from '@app/services/user-group/user-group.service';
+import {BsModalService} from 'ngx-bootstrap';
 
 @Component({
   selector: 'app-user-group-list',
@@ -17,15 +17,16 @@ import { BsModalService } from 'ngx-bootstrap';
 export class UserGroupListComponent implements OnInit, OnDestroy {
   filterOptions: FilterOptions = new FilterOptions();
   userGroups: UserGroup[] = [];
-  filterKeyUpSubscribe: Subscription;
   isInit: boolean = true;
   resultCount: number;
+
   private mainSubscription = new Subscription();
 
   fields = [
-    { label: 'Group Name', value: 'name' },
-    { label: 'Number of users', value: 'users' },
+    {label: 'Group Name', value: 'name'},
+    {label: 'Number of users', value: 'users'},
   ];
+
   constructor(
     private userGroupService: UserGroupService,
     private route: ActivatedRoute,
@@ -36,19 +37,22 @@ export class UserGroupListComponent implements OnInit, OnDestroy {
   @ViewChild('globalFilter') globalFilterElement: ElementRef;
 
   ngOnInit() {
-    const routerSubscription = this.route.data.subscribe((data: Data) => {
-      this.userGroups = data['groups'].items;
-      this.resultCount = data['groups'].totalCount;
-    });
-    const filterSubscription = this.filterKeyUpSubscribe = fromEvent(
+    const routerSubscription = this.route.data
+      .subscribe((data: Data) => {
+        this.userGroups = data['groups'].items;
+        this.resultCount = data['groups'].totalCount;
+      });
+
+    const filterSubscription = fromEvent(
       this.globalFilterElement.nativeElement,
       'keyup'
     )
-      .pipe(debounceTime(300))
-      .subscribe(() => {
-        this.filterOptions.page = 1;
-        this.onDataLoad();
-      });
+      .pipe(
+        debounceTime(300),
+        tap(() => this.filterOptions.page = 1),
+        switchMap(() => this.loadGroups())
+      ).subscribe();
+
     this.mainSubscription.add(filterSubscription);
     this.mainSubscription.add(routerSubscription);
   }
@@ -57,27 +61,30 @@ export class UserGroupListComponent implements OnInit, OnDestroy {
   openCreateModal() {
     const modal = this.modalService.show(UserGroupCreateModalComponent);
     const onCloseSubscription = modal.content.onClose
-      .pipe(switchMap((userGroupData: UserGroupDataInterface) => this.userGroupService.createUserGroup(userGroupData)))
-      .subscribe(() => this.onDataLoad());
+      .pipe(
+        switchMap((userGroupData: UserGroupDataInterface) => this.userGroupService.createUserGroup(userGroupData)),
+        switchMap(() => this.loadGroups())
+      ).subscribe();
 
     this.mainSubscription.add(onCloseSubscription);
   }
 
 
-  onDataLoad() {
-    const getAllGroupSubscription = this.userGroupService.getAllGroups(null, this.filterOptions).subscribe(groups => {
-      this.userGroups = groups.items;
-      this.resultCount = groups.totalCount;
-    });
-    this.mainSubscription.add(getAllGroupSubscription);
+  loadGroups() {
+    return this.userGroupService.getAllGroups(null, this.filterOptions)
+      .pipe(tap(groups => {
+        this.userGroups = groups.items;
+        this.resultCount = groups.totalCount;
+      }));
   }
 
-  // editGroup(index) {
-  //   this.upsertGroup(this.userGroups[index], true);
-  // }
+  editGroup(index: number) {
+    console.warn('Not implemented yet!');
+    //   this.upsertGroup(this.userGroups[index], true);
+  }
 
   deleteGroup(id: string): void {
-    console.log(id);
+    console.warn('Not implemented yet!');
   }
 
   loadGroupLazy(event: any): void {
@@ -96,7 +103,10 @@ export class UserGroupListComponent implements OnInit, OnDestroy {
     }
     this.filterOptions.page = page;
     this.filterOptions.sort = sort;
-    this.onDataLoad();
+
+    const loadGroupsSubscription = this.loadGroups().subscribe();
+
+    this.mainSubscription.add(loadGroupsSubscription);
   }
 
   ngOnDestroy(): void {
