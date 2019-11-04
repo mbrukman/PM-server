7ab58e70800f4d3482634135962e5170a4edec15
arispen <arispen@gmail.com>
2019-11-04 20:45:54 +0100

    use MustMatch validator

--- console/src/app/users-management/users-list/create-user/create-user.component.ts
+++ console/src/app/users-management/users-list/create-user/create-user.component.ts
@@ -3,36 +3,32 @@ import { FormGroup, FormControl, Validators, FormBuilder } from '@angular/forms'
 import { BsModalRef } from 'ngx-bootstrap';
 import { User } from '@app/users-management/models/user.model';
 import { Subject } from 'rxjs';
+import { MustMatch } from '@app/shared/must-match.validator';
 
 @Component({
   selector: 'app-create-user',
   templateUrl: './create-user.component.html',
   styleUrls: ['./create-user.component.scss']
 })
-export class CreateUserComponent {
+export class CreateUserComponent implements OnInit {
+
+  createUserForm: FormGroup;
 
   constructor(public bsModalRef: BsModalRef, private formBuilder: FormBuilder) {}
 
-  public createUserForm = this.formBuilder.group({
-    name: ['', [Validators.required]],
-    email: ['', [Validators.required, Validators.email]],
-    phoneNumber: '',
-    password: ['', [Validators.required, Validators.pattern('^(?=.*[0-9]+.*)(?=.*[a-zA-Z]+.*)[0-9a-zA-Z]{8,}$')]],
-    confirmPassword: ['', [Validators.required]],
-    changePasswordOnNextLogin: false,
-  }, {validator: this.checkPasswords });
-
   public onClose = new Subject<User>();
 
-  get email() { return this.createUserForm.get('email'); }
-  get password() { return this.createUserForm.get('password'); }
-  get confirmPassword() { return this.createUserForm.get('confirmPassword'); }
+  get form() { return this.createUserForm.controls; }
 
-  checkPasswords(group: FormGroup) {
-    const password = group.get('password').value;
-    const confirmPassword = group.get('confirmPassword').value;
-
-    return password === confirmPassword ? null : { notSame: true };
+  ngOnInit() {
+    this.createUserForm = this.formBuilder.group({
+      name: ['', [Validators.required]],
+      email: ['', [Validators.required, Validators.email]],
+      phoneNumber: '',
+      password: ['', [Validators.required, Validators.pattern('^(?=.*[0-9]+.*)(?=.*[a-zA-Z]+.*)[0-9a-zA-Z]{8,}$')]],
+      confirmPassword: ['', [Validators.required]],
+      changePasswordOnNextLogin: false,
+    }, {validator: MustMatch('password', 'confirmPassword') });
   }
 
   onCreateUser() {
