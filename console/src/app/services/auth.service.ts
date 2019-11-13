9dc71737ff0e3041fddf88f91d60e9af454a4e69
arispen <arispen@gmail.com>
2019-11-13 15:55:19 +0100

    auth service rethinked

--- console/src/app/services/auth.service.ts
+++ console/src/app/services/auth.service.ts
@@ -1,36 +1,34 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { BehaviorSubject, Observable } from 'rxjs';
-import { map } from 'rxjs/operators';
+import { HttpClient, HttpResponse } from '@angular/common/http';
+import { Observable } from 'rxjs';
 import { User } from './users/user.model';
+import { switchMap, tap, map } from 'rxjs/operators';
 
 @Injectable({ providedIn: 'root' })
 export class AuthService {
-  private currentUserSubject: BehaviorSubject<User>;
-  public currentUser: Observable<User>;
 
   constructor(private http: HttpClient) {
-    this.currentUserSubject = new BehaviorSubject<User>(JSON.parse(localStorage.getItem('currentUser')));
-    this.currentUser = this.currentUserSubject.asObservable();
   }
 
-  public get currentUserValue(): User {
-    return this.currentUserSubject.value;
+  static get token(): string {
+    return localStorage.getItem('access_token');
   }
 
-  login(username: string, password: string): Observable<User> {
-    return this.http.post<User>(`api/users/authenticate`, { username, password })
-      .pipe(map(user => {
-        // store user details and jwt token in local storage to keep user logged in between page refreshes
-        localStorage.setItem('currentUser', JSON.stringify(user));
-        this.currentUserSubject.next(user);
-        return user;
-      }));
+  private saveToken(token: string) {
+    localStorage.setItem('access_token', token);
+  }
+
+  public login(email: string, password: string): Observable<User> {
+    return this.http.post<User>(`api/users/authenticate`, { email, password }, { observe: 'response' })
+      .pipe(
+        tap((response: HttpResponse<User>) => {
+          this.saveToken(response.headers.get('authorization'));
+        }),
+        map(response => response.body as User)
+      );
   }
 
   logout() {
-    // remove user from local storage to log user out
-    localStorage.removeItem('currentUser');
-    this.currentUserSubject.next(null);
+    localStorage.removeItem('access_token');
   }
 }
