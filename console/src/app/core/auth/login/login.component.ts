566fb49435a0f800be01cd5b62ea7824922c8369
arispen <arispen@gmail.com>
2019-11-13 16:53:14 +0100

    login component class

--- console/src/app/core/auth/login/login.component.ts
+++ console/src/app/core/auth/login/login.component.ts
@@ -1,19 +1,48 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { AuthService } from '@app/services/auth.service';
+import { User } from '@app/services/users/user.model';
+import { Router } from '@angular/router';
+import { HttpErrorResponse } from '@angular/common/http';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
-export class LoginComponent implements OnInit {
+export class LoginComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  email: string;
+  password: string;
+  errorMessage: string;
+  subscription: Subscription;
+
+  constructor(private authService: AuthService, private router: Router) { }
 
   ngOnInit() {
+
   }
 
-  onLogin() {
-    throw new Error('not implemented');
+  login() {
+    this.subscription = this.authService.login(this.email, this.password)
+      .subscribe(
+        (user: User) => {
+          console.log('User logged in:', user);
+          this.router.navigateByUrl('/');
+        },
+        (error: HttpErrorResponse) => {
+          console.error(error);
+          if (error.status === 0) {
+            this.errorMessage = 'Error connecting to server.';
+            return;
+          }
+          this.errorMessage = error.message;
+        }
+      );
+  }
+
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
   }
 
 }
