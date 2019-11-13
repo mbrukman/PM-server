aaaa19f887175ed2157ad24204279756b5905933
Chris Bernat <bernat.christopher@gmail.com>
2019-11-13 18:00:15 +0100

    finished feature

--- console/src/app/users-management/users-management.module.ts
+++ console/src/app/users-management/users-management.module.ts
@@ -2,10 +2,6 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { UsersManagementComponent } from './users-management.component';
 import { UsersManagementRoutingModule } from './users-management-routing.module';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { UsersListComponent } from './users-list/users-list.component';
-import { SharedModule } from '@shared/shared.module';
-import { TooltipModule } from 'ngx-bootstrap/tooltip';
 import { UsersListResolver } from './resolvers/users-list.resolver';
 import { UserGroupModule } from '@app/users-management/user-group/user-group.module';
 import { UserGroupResolver } from './resolvers/user-group.resolver';
@@ -22,10 +18,6 @@ import {IAMUserModule} from '@app/users-management/iam-user/iam-user.module';
   ],
   declarations: [
     UsersManagementComponent,
-    UsersListComponent,
-    CreateUserComponent,
-    // ManageUserComponent,
-    // EditUserComponent,
   ],
   providers: [UsersListResolver, UserGroupResolver]
 })
