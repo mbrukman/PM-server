3f4d7b3e1b2b9511ff8ac0f9767f453d2acfbdce
arispen <arispen@gmail.com>
2019-11-21 10:00:47 +0100

    update policy view based on permissions in model

--- console/src/app/shared/inputs/checkbox/checkbox.component.ts
+++ console/src/app/shared/inputs/checkbox/checkbox.component.ts
@@ -12,16 +12,22 @@ export class CheckboxComponent extends BasicInputComponent implements OnInit, On
   @ViewChild('checkboxInput') checkboxInput: ElementRef;
   private subscription: Subscription;
 
-  @Input() label: string;
+  @Input() label?: string;
   @Input() toggle?: Subject<boolean>;
+  @Input() checked?: boolean;
 
   constructor() {
     super();
   }
 
   ngOnInit() {
+    if (this.checked) {
+      this.checkboxInput.nativeElement.checked = this.checked;
+    }
+
     this.subscription = fromEvent(this.checkboxInput.nativeElement, 'change')
       .subscribe((e: ChangeEvent) => this.onChange.next((e.target as HTMLInputElement).checked as boolean));
+
     if (this.toggle) {
       this.subscription.add(this.toggle.subscribe(newValue => {
         this.checkboxInput.nativeElement.checked = newValue;
--- console/src/app/users-management/policies/policy/policy.component.ts
+++ console/src/app/users-management/policies/policy/policy.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 import { Subject } from 'rxjs';
 
 @Component({
@@ -10,11 +10,17 @@ export class PolicyComponent implements OnInit {
 
   public toggleCheckboxes: Subject<boolean> = new Subject<boolean>();
 
-  public policyValue: Policy = { create: false, read: false, update: false, remove: false };
-
+  // public policyValue: Policy = { create: false, read: false, update: false, remove: false };
+  @Input() name: String;
+  @Input() policyValue: Policy = { create: false, read: false, update: false, remove: false };
   constructor() { }
 
+  get wholePolicyTrue() {
+    return this.policyValue.create && this.policyValue.read && this.policyValue.update && this.policyValue.remove;
+  }
+
   ngOnInit() {
+    console.log(this.policyValue);
   }
 
   toggleWholePolicy(newValue: boolean) {
@@ -24,6 +30,10 @@ export class PolicyComponent implements OnInit {
         this.policyValue[policy] = newValue;
       }
     }
+  }
+
+  updateCheckboxes(newValue: Policy) {
+
   }
 
   togglePermission(permissionName: string, newValue: boolean) {
