34739683ba501648d7f7923f561a714abab9bbe9
arispen <arispen@gmail.com>
2019-11-22 09:12:17 +0100

    add missing iamPolicy in UserGroup constructor

--- console/src/app/services/user-group/user-group.model.ts
+++ console/src/app/services/user-group/user-group.model.ts
@@ -12,11 +12,12 @@ export default class UserGroup {
 
   // tslint:disable-next-line: variable-name
   constructor(userGroup = {} as UserGroup) {
-    const { _id, name, description, users } = userGroup;
+    const { _id, name, description, users, iamPolicy } = userGroup;
     this._id = _id;
     this.name = name;
     this.description = description;
     this.users = this.mapUsers(users);
+    this.iamPolicy = iamPolicy;
   }
   private mapUsers(users = [] as Array<User | string>) {
     return users.map(user => {
