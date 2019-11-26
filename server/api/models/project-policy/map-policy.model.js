928a4c92d788e14fd131b0b6e30179aef6e1f49f
Chris Bernat <bernat.christopher@gmail.com>
2019-11-26 16:16:21 +0100

    fixed default values for permissions

--- server/api/models/project-policy/map-policy.model.js
+++ server/api/models/project-policy/map-policy.model.js
@@ -6,10 +6,10 @@ module.exports.MapPolicySchema = new Schema({
   map: { type: mongoose.Schema.Types.ObjectId, ref: "Map" },
   permissions: {
     read: { type: Boolean, default: true },
-    update: { type: Boolean, default: true },
-    delete: { type: Boolean, default: true },
-    execute: { type: Boolean, default: true },
-    archive: { type: Boolean, default: true }
+    update: { type: Boolean, default: false },
+    delete: { type: Boolean, default: false },
+    execute: { type: Boolean, default: false },
+    archive: { type: Boolean, default: false }
   }
 });
 
--- server/api/models/project-policy/project-policy.model.js
+++ server/api/models/project-policy/project-policy.model.js
@@ -6,10 +6,10 @@ module.exports.ProjectPolicySchema = new Schema({
   project: { type: mongoose.Schema.Types.ObjectId, ref: "Project" },
   permissions: {
     read: { type: Boolean, default: true },
-    createMap: { type: Boolean, default: true },
-    update: { type: Boolean, default: true },
-    delete: { type: Boolean, default: true },
-    archive: { type: Boolean, default: true }
+    createMap: { type: Boolean, default: false },
+    update: { type: Boolean, default: false },
+    delete: { type: Boolean, default: false },
+    archive: { type: Boolean, default: false }
   },
   maps: [{ type: mongoose.Schema.Types.ObjectId, ref: "MapPolicy" }]
 });
