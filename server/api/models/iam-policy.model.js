e368a1eade36bb801b64bdefaf8ae74c9da287bb
Chris Bernat <bernat.christopher@gmail.com>
2019-11-26 16:16:48 +0100

    fixed default values for permissions

--- server/api/models/iam-policy.model.js
+++ server/api/models/iam-policy.model.js
@@ -4,9 +4,9 @@ const Schema = mongoose.Schema;
 const iamPolicySchema = new Schema({
   permissions: {
     read: { type: Boolean, default: true },
-    create: { type: Boolean, default: true },
-    update: { type: Boolean, default: true },
-    remove: { type: Boolean, default: true }
+    create: { type: Boolean, default: false },
+    update: { type: Boolean, default: false },
+    remove: { type: Boolean, default: false }
   },
   user: { type: Schema.Types.ObjectId, ref: "User" },
   group: { type: Schema.Types.ObjectId, ref: "UserGroup" }
